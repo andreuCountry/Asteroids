9ef7a56471c27aa7d0c98103027be9b56f6f11da
sanchezcas <sanchezcas@esatvalencia.lan>
2026-03-09 19:29:16 +0100

    add main

---
+++ main.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <esat/window.h>
+#include <esat/draw.h>
+#include <esat/sprite.h>
+#include <esat/input.h>
+#include <esat/time.h>
+#include "structs.h"
+#include "sprites/sprites.h"
+
+double current_time, last_time;
+int fps = 60;
+
+float windowX = 800.0f, windowY = 608.0f;
+
+int esat::main(int argc, char **argv) {
+
+    
+
+    esat::WindowInit(windowX, windowY);
+    esat::WindowSetMouseVisibility(true);
+
+    while (esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
+        last_time = esat::Time();
+
+        esat::DrawBegin();
+        esat::DrawClear(0, 10, 0);
+
+
+
+        esat::DrawEnd();
+
+        do { current_time = esat::Time(); }
+        while ((current_time - last_time) <= 1000.0 / fps);
+
+        esat::WindowFrame();
+    }
+
+
+    esat::WindowDestroy();
+    return 0;
+}
