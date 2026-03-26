f95a3e559a345987b83fad7a76ecbd734e07a7ea
andreuCountry <132867325+andreuCountry@users.noreply.github.com>
2026-03-26 17:30:11 +0100

    Update helen.cc, including speed

--- helen.cc
+++ helen.cc
@@ -7,6 +7,7 @@
 #include <esat/math.h>
 #include <esat/time.h>
 #include <math.h>
+#include <mathlib.h>
 
 const int numPoints = 5, pi = 3.141592f;
 
@@ -21,6 +22,11 @@ esat::Vec3 g_figurita[numPoints] {
 float DegreeToRadians(float degree) {
     return degree * pi / 180.0f;
 }
+// Posicion de la naves
+esat::Vec3 currentPosition {400, 300, 1};
+esat::Vec3 speed = {0.2f, 0.2f, 0};
+// Inicialización chapucera
+esat::Vec3 newPosition = {0, 0, 0};
 
 void InitShip() {
     g_figurita[0] = {cosf(DegreeToRadians(0.0f)) * 50, sinf(DegreeToRadians(0.0f)) * 50, 1.0f};
@@ -28,6 +34,20 @@ void InitShip() {
     g_figurita[2] = {cosf(DegreeToRadians(170.0f)) * 15, sinf(DegreeToRadians(170.0f)) * 15, 1.0f};
     g_figurita[3] = {cosf(DegreeToRadians(-170.0f)) * 15, sinf(DegreeToRadians(-170.0f)) * 15, 1.0f};
     g_figurita[4] = {cosf(DegreeToRadians(-160.0f)) * 20, sinf(DegreeToRadians(-160.0f)) * 20, 1.0f};
+}
+
+void Movement() {
+    if (esat::IsSpecialKeyPressed(esat::kSpecialKey_Right)) {
+        
+        esat::Vec3 newPosition = {currentPosition.x + speed.x, currentPosition.y, 1};
+        currentPosition = newPosition;
+    }
+
+    if (esat::IsSpecialKeyPressed(esat::kSpecialKey_Down)) {
+        
+        esat::Vec3 newPosition = {currentPosition.x, currentPosition.y + speed.y, 1};
+        currentPosition = newPosition;
+    }
 }
 
 esat::Mat3 UpdateFigurita(esat::Vec2 scale, float angle, esat::Vec2 whereMove) {
@@ -47,7 +67,9 @@ void DrawFigurita(esat::Mat3 m, int numberOfFigures) {
 
     float points[numPoints * 2];
 
-    m = esat::Mat3Multiply(esat::Mat3Translate(esat::MousePositionX(), esat::MousePositionY()), m);
+    m = esat::Mat3Multiply(esat::Mat3Translate(currentPosition.x, currentPosition.y), m);
+
+    Movement();
 
     for (int i = 0; i < numPoints; i++) {
         // Necesitamos esto para transformar los Mat3 en Vec3, para dibujar
@@ -55,6 +77,8 @@ void DrawFigurita(esat::Mat3 m, int numberOfFigures) {
         points[i*2] = tmp.x;
         points[i*2+1] = tmp.y;
     }
+
+    // Checkeito del movimiento de la nave
     esat::DrawSolidPath(points, numPoints);
 }
 
